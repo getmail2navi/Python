e40264d1a5cbd27ffa977bbbd904bd9a114464c7
Naveen Kumar <34242443+getmail2navi@users.noreply.github.com>
2018-01-24 15:00:09 +0530

    Add files via upload

---
+++ listoperation.py
@@ -0,0 +1,38 @@
+Lists Operation :)
+>>> SCA = ["BCA","MCA"]
+>>> SCA
+['BCA', 'MCA']
+
+NAME = ["Naveen",29,"MCA"]
+>>> NAME
+['Naveen', 29, 'MCA']
+
+//add data in list
+>>> cap.append('MSC')
+>>> cap
+['MCA', 'MCA2', 'MSC']
+
+//empty list
+>>> empty=[]
+>>> empty.append(1)
+>>> empty
+[1]
+
+//list operation
+
+>>> cap[:3]
+['MCA', 'MCA2', 'MSC']
+>>> cap[0]
+'MCA'
+>>> 
+>>> 
+>>> 
+>>> cap[-1]
+'MSC'
+
+//List Inside List :)
+
+>>> x = [cap,NAME]
+>>> x
+[['MCA', 'MCA2', 'MSC'], ['NAveen', 11614645, 'MCA']]
+
