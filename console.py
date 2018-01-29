e51286bb6ade9180bfc0bf3df63c3ff79996d1ba
Naveen Kumar <34242443+getmail2navi@users.noreply.github.com>
2018-01-29 10:58:52 +0530

    Add files via upload

---
+++ console.py
@@ -0,0 +1,49 @@
+>>> num = input("enter your number")
+enter your number10
+>>> num
+'10'
+
+>>> number = int(input("write number"))
+write number12
+>>>number
+12
+
+
+Num= int(input("enter your number"))
+enter your number 11
+>>> Num
+11
+
+>>> print("your num is",Num)
+your num is 11
+
+//
+ abc= str(input("enter your subject"))
+enter your subject python
+>>> print("your subject is",abc)
+your subject is  python
+
+
+//
+ classname = input("write your class name")
+write your class name mca
+>>> print("your class name is",classname)
+ypur class name is  mca
+
+//add number
+
+>>> num1 = int(input("write number"))
+write number12
+>>> num2 = int(input("write number"))
+write number13
+>>> print (num1+num2)
+25
+
+>>> print (num1*num2)
+156
+
+//
+>>> print ("my class name is",classname,"and my number is",num1)
+my class name is  mca and my number is 12
+
+odd loop when the number of niput is not define
